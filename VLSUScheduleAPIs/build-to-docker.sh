04403a731fe140da688e97058ba3d292f6d951b0
maxim <zim.ma2010@yandex.ru>
2019-09-07 16:12:01 +0300

    Docker + Start settings

---
+++ VLSUScheduleAPIs/build-to-docker.sh
@@ -0,0 +1,9 @@
+rm -r AuthAPI/Commonlibrary/
+rm -r IntegrationAPI/Commonlibrary/
+rm -r VLSUScheduleAPIs/Commonlibrary/
+
+cp -r Commonlibrary AuthAPI/
+cp -r Commonlibrary IntegrationAPI/
+cp -r Commonlibrary VLSUScheduleAPIs/
+
+docker-compose build
