4d5b71067cf664598caa298f05aa87fcf15af4d6
maxim <zim.ma2010@yandex.ru>
2019-09-08 12:31:49 +0300

    Init

--- VLSUScheduleAPIs/VLSUScheduleAPIs/VLSUScheduleAPIs/Startup.cs
+++ VLSUScheduleAPIs/VLSUScheduleAPIs/VLSUScheduleAPIs/Startup.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.Swagger;
-using VLSUScheduleAPI.Services;
 
 namespace VLSUScheduleAPIs
 {
@@ -29,24 +26,6 @@ namespace VLSUScheduleAPIs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddDbContext<DatabaseContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info
-                {
-                    Version = "v1",
-                    Title = "VLSUScheduleApi",
-                    Description = "VLSUScheduleApi"
-                });
-            });
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(x =>
-                {
-                    x.Authority = Configuration.GetValue<string>("AuthService");
-                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,14 +35,14 @@ namespace VLSUScheduleAPIs
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseAuthentication();
-            app.UseMvc();
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
+            else
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "VLSUScheduleApi");
-            });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
